92f93e484f01174d9b72d41df77d786c03723642
Petr Slepicka <petr.slepicka@radixal.net>
2021-02-15 14:24:21 +0100

    Error code for get-stack-output action

--- read-stack-output/index.js
+++ read-stack-output/index.js
@@ -6,6 +6,7 @@ var cloudFormation = new AWS.CloudFormation();
 cloudFormation.describeStacks({ StackName: STACK_NAME }, (error, data) => {
     if (error) {
         console.log(error);
+        core.setFailed("An error occured while reading stack output.");
     } else {
         data.Stacks[0].Outputs.forEach(output => {
             //core.exportVariable(STACK_NAME + "." + output.OutputKey, output.OutputValue);
