5ab74ffd7a73d623635038e1036320426f286abe
Petr Slepicka <petr.slepicka@radixal.net>
2021-02-18 14:16:13 +0100

    async

--- setup-aws-credentials/index.js
+++ setup-aws-credentials/index.js
@@ -1,47 +1,49 @@
 const core = require("@actions/core");
 const aws = require('aws-sdk');
 
-try {
-    let AWS_ENVIRONMENT = core.getInput('AWS_ENVIRONMENT');
-    let AWS_ACCESS_KEY_ID = null;
-    let AWS_SECRET_ACCESS_KEY = null;
-    let AWS_REGION = core.getInput("AWS_REGION");
+async function run() {
+    try {
+        let AWS_ENVIRONMENT = core.getInput('AWS_ENVIRONMENT');
+        let AWS_ACCESS_KEY_ID = null;
+        let AWS_SECRET_ACCESS_KEY = null;
+        let AWS_REGION = core.getInput("AWS_REGION");
 
-    switch (AWS_ENVIRONMENT) {
-        case "DEV": {
-            AWS_ACCESS_KEY_ID = core.getInput("DEV_AWS_ACCESS_KEY_ID");
-            AWS_SECRET_ACCESS_KEY = core.getInput("DEV_AWS_SECRET_ACCESS_KEY");
-            break;
+        switch (AWS_ENVIRONMENT) {
+            case "DEV": {
+                AWS_ACCESS_KEY_ID = core.getInput("DEV_AWS_ACCESS_KEY_ID");
+                AWS_SECRET_ACCESS_KEY = core.getInput("DEV_AWS_SECRET_ACCESS_KEY");
+                break;
+            }
+            case "UAT": {
+                AWS_ACCESS_KEY_ID = core.getInput("UAT_AWS_ACCESS_KEY_ID");
+                AWS_SECRET_ACCESS_KEY = core.getInput("UAT_AWS_SECRET_ACCESS_KEY");
+                break;
+            }
+            case "PROD": {
+                AWS_ACCESS_KEY_ID = core.getInput("PROD_AWS_ACCESS_KEY_ID");
+                AWS_SECRET_ACCESS_KEY = core.getInput("PROD_AWS_SECRET_ACCESS_KEY");
+                break;
+            }
+            default: throw "Unknown environment '" + AWS_ENVIRONMENT + "'.";
         }
-        case "UAT": {
-            AWS_ACCESS_KEY_ID = core.getInput("UAT_AWS_ACCESS_KEY_ID");
-            AWS_SECRET_ACCESS_KEY = core.getInput("UAT_AWS_SECRET_ACCESS_KEY");
-            break;
-        }
-        case "PROD": {
-            AWS_ACCESS_KEY_ID = core.getInput("PROD_AWS_ACCESS_KEY_ID");
-            AWS_SECRET_ACCESS_KEY = core.getInput("PROD_AWS_SECRET_ACCESS_KEY");
-            break;
-        }
-        default: throw "Unknown environment '" + AWS_ENVIRONMENT + "'.";
+
+        console.log("AWS_ENVIRONMENT: " + AWS_ENVIRONMENT);
+        console.log("AWS_ACCESS_KEY_ID: " + AWS_ACCESS_KEY_ID);
+        console.log("AWS_SECRET_ACCESS_KEY: " + AWS_SECRET_ACCESS_KEY);
+        console.log("AWS_REGION: " + AWS_REGION);
+
+        core.setSecret(accessKeyId);
+        core.exportVariable('AWS_ACCESS_KEY_ID', accessKeyId);
+        core.setSecret(secretAccessKey);
+        core.exportVariable('AWS_SECRET_ACCESS_KEY', secretAccessKey);
+        core.exportVariable('AWS_DEFAULT_REGION', region);
+        core.exportVariable('AWS_REGION', region);
+
+        await validateCredentials(AWS_ACCESS_KEY_ID);
+    } catch (error) {
+        console.log("error: ", error);
+        core.setFailed(error.message);
     }
-
-    console.log("AWS_ENVIRONMENT: " + AWS_ENVIRONMENT);
-    console.log("AWS_ACCESS_KEY_ID: " + AWS_ACCESS_KEY_ID);
-    console.log("AWS_SECRET_ACCESS_KEY: " + AWS_SECRET_ACCESS_KEY);
-    console.log("AWS_REGION: " + AWS_REGION);
-
-    core.setSecret(accessKeyId);
-    core.exportVariable('AWS_ACCESS_KEY_ID', accessKeyId);
-    core.setSecret(secretAccessKey);
-    core.exportVariable('AWS_SECRET_ACCESS_KEY', secretAccessKey);
-    core.exportVariable('AWS_DEFAULT_REGION', region);
-    core.exportVariable('AWS_REGION', region);
-
-    await validateCredentials(AWS_ACCESS_KEY_ID);
-} catch (error) {
-    console.log("error: ", error);
-    core.setFailed(error.message);
 }
 
 
@@ -75,4 +77,12 @@ async function validateCredentials(expectedAccessKeyId) {
     if (expectedAccessKeyId && expectedAccessKeyId != actualAccessKeyId) {
         throw new Error('Unexpected failure: Credentials loaded by the SDK do not match the access key ID configured by the action');
     }
+}
+
+
+module.exports = run;
+
+/* istanbul ignore next */
+if (require.main === module) {
+    run();
 }
